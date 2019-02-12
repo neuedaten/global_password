bbfa7b26d10959a42e6f44dbd62f763e942fdb2f
Bastian Schwabe <bas@neuedaten.de>
2019-02-12 15:11:01 +0100

    fix depends

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '~9.5.3',
+            'typo3' => '9.5.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
