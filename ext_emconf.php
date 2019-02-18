e38a24d244e012202419ce64f6c47dff65c83f22
Bastian Schwabe <bas@neuedaten.de>
2019-02-18 11:38:40 +0100

    update description / update version to 0.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Global password',
-    'description' => 'Login form to protect a complete TYPO3 System from unauthorized visitors. Useful for development and staging servers.',
+    'description' => 'Password protection for a complete TYPO3 frontend. Useful for development and staging servers.',
     'category' => 'frontend',
     'state' => 'beta',
     'uploadfolder' => 0,
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Bastian Schwabe',
     'author_email' => 'bas@neuedaten.de',
     'author_company' => 'NEUEDATEN',
-    'version' => '0.0.1',
+    'version' => '0.1.0',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.5.99',
