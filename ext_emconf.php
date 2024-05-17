b8be447d38843ea6140b223cd149025399b0fbeb
Mike Street <mikestreety@gmail.com>
2024-05-17 10:20:12 +0100

    build: Version 2 is not compatible with v9
    The site attibute isn't in the request

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-11.5.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
